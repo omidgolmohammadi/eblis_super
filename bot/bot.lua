299db2d96e60d47dd068d6d0bda08640291813bb
omidgolmohammadi <omidgolmohammadi@users.noreply.github.com>
2016-04-14 18:41:27 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {172442430},
+    sudo_users = {189683919},
     admin_users = {},
     disabled_channels = {}
   }
